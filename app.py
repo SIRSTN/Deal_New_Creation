a8974eb488c4abce9fc6306d98bb254b7727d06d
SIRSTN <stephane.velen@gmail.com>
2024-03-05 15:18:40 +0100

    cloud commit

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from pymongo import MongoClient
 from datetime import datetime
+from configparser import ConfigParser
 
-def connect_to_mongodb(uri="mongodb://localhost:27017/"):
-    client = MongoClient(uri)
-    return client['Deal_Transactions']
+# Load configuration file
+config = ConfigParser()
+config.read('config.ini')
+
+# Setup MongoDB Client
+#client = MongoClient(config.get('DEAL_NEW_CREATION', 'MongoClient'))
+client = MongoClient("mongodb://localhost:27017/")
+db = client['Deal_Transactions']
 
 def insert_deal_and_valuation(db, deal_id, keyword, date, volume, price, amount):
     # Insert into Deals collection
@@ -26,6 +32,9 @@ def insert_deal_and_valuation(db, deal_id, keyword, date, volume, price, amount)
         "Volume": volume,
         "Price": price,
         "Amount": amount,
+        "Init_Volume": volume,
+        "Sold_Volume": 0,
+        "Sold_Amount": 0,
     }
     db.Valuations.insert_one(valuation)
 
@@ -40,7 +49,13 @@ def insert_deal_and_valuation(db, deal_id, keyword, date, volume, price, amount)
     }
     db.Transactions.insert_one(transaction)
 
-# Example usage
-db = connect_to_mongodb()
-
-insert_deal_and_valuation(db, 1, "Bitcoin", "2023-01-01", 0.01, 28000, 280)
+insert_deal_and_valuation(db, 1, "Bitcoin", "2023-01-01", 0.01, 25000, 250)
+insert_deal_and_valuation(db, 2, "Bitcoin", "2023-01-01", 0.01, 25500, 255)
+insert_deal_and_valuation(db, 3, "Bitcoin", "2023-01-01", 0.01, 26000, 260)
+insert_deal_and_valuation(db, 4, "Bitcoin", "2023-01-01", 0.01, 26500, 265)
+insert_deal_and_valuation(db, 5, "Bitcoin", "2023-01-01", 0.01, 27000, 270)
+insert_deal_and_valuation(db, 6, "Bitcoin", "2023-01-01", 0.01, 27500, 275)
+insert_deal_and_valuation(db, 7, "Bitcoin", "2023-01-01", 0.01, 28000, 280)
+insert_deal_and_valuation(db, 8, "Bitcoin", "2023-01-01", 0.01, 28500, 285)
+insert_deal_and_valuation(db, 9, "Bitcoin", "2023-01-01", 0.01, 29000, 290)
+insert_deal_and_valuation(db, 10, "Bitcoin", "2023-01-01", 0.01, 29500, 295)
